6fa74c4d3e12c040232834387c393e4e22ed8f6d
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2021-02-28 02:57:30 -0300

    add anonymous mode for timer util and typing for utils funcs

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,24 @@
-from psutil import NoSuchProcess, AccessDenied
+from psutil import NoSuchProcess, AccessDenied, Process
 from win32con import SM_CXICON
 from win32api import GetSystemMetrics
 from os.path import dirname, abspath
 from os.path import join as p_join
 from os.path import exists as p_exists
 from win32ui import CreateDCFromHandle, CreateBitmap
-from win32gui import ExtractIconEx, DestroyIcon, GetDC
+from win32gui import ExtractIconEx, DestroyIcon, GetDC # noqa
 from PIL import Image
 from subprocess import Popen, PIPE
 from os import PathLike, getpid
-from typing import Union
+from typing import Union, Dict, Callable, Any, List, Optional, Type, Collection
 from pywintypes import error # noqa
 from bisect import bisect_left, bisect_right
 import sys
 from time import perf_counter
 
-this_dir = getattr(sys, '_MEIPASS', abspath(dirname(__file__)))
 this_pid = getpid()
 path_type = Union[str, bytes, PathLike]
-default_icon_path = p_join(this_dir, 'icons', 'default.png')
+this_dir: path_type = getattr(sys, '_MEIPASS', abspath(dirname(__file__)))
+default_icon_path: path_type = p_join(this_dir, 'icons', 'default.png')
 
 
 def icon_path(exe: path_type, name: str):
@@ -68,13 +68,13 @@ def is_responding(pid: int):
     return str(pid) in str(status)
 
 
-def keyring_bisect_left(seq, item, key_func, reverse=False):
+def keyring_bisect_left(seq: Collection, item, key_func: Callable[[Any], Any], reverse=False):
     k = key_func(item)
     keys = [key_func(e) for e in seq]
     return ordering_bisect_left(keys, k, reverse)
 
 
-def ordering_bisect_left(seq, e, reverse, lo=None, hi=None):
+def ordering_bisect_left(seq: Collection, e, reverse: bool, lo: Optional[int] = None, hi: Optional[int] = None):
     """Find first index, starting from left, to insert e."""
     lo = lo or 0
     hi = hi or len(seq)
@@ -84,7 +84,7 @@ def ordering_bisect_left(seq, e, reverse, lo=None, hi=None):
         return bisect_left(seq, e, lo, hi)
 
 
-def kill_proc_tree(parent, include_parent=True):
+def kill_proc_tree(parent: Type[Process], include_parent=True):
     fails = list()
     try:
         children = parent.children(recursive=True)
@@ -98,7 +98,7 @@ def kill_proc_tree(parent, include_parent=True):
     return fails
 
 
-def kill(proc):
+def kill(proc: Type[Process]):
     if proc.pid != this_pid:
         try:
             proc.kill()
@@ -111,18 +111,37 @@ def kill(proc):
         return False
 
 
-funcs_results = dict()
+funcs_results: Dict[Callable[[Any], Any], float] = dict()
+custom_results: Dict[str, List[Optional[Union[float, int]]]] = dict()
 
 
-def timer(function):
+def timer(function: Union[Callable[[Any], Any], str]) -> Optional[Callable[[Any], Any]]:
+    if type(function) == str:
+        if function in custom_results:
+            tic = custom_results[function][0]
+            toe = custom_results[function][1]
+            if tic is None:
+                custom_results[function][0] = perf_counter()
+            elif toe is None:
+                custom_results[function][1] = perf_counter() - tic
+            else:
+                custom_results[function][1] = (perf_counter() - tic + toe) / 2
+            if tic is not None:
+                custom_results[function][0] = None
+                print(f'"{function}" is taking about {custom_results[function][1]:.5f} seconds.')
+        else:
+            custom_results[function] = [None, None]
+        return
+
     def new_func(*args, **kwargs):
-        tic = perf_counter()
-        function(*args, **kwargs)
+        _tic = perf_counter()
+        to_return = function(*args, **kwargs)
         tac = perf_counter()
         if function in funcs_results:
-            toe = (tac - tic + funcs_results[function]) / 2
+            _toe = (tac - _tic + funcs_results[function]) / 2
         else:
-            toe = tac - tic
-        funcs_results[function] = toe
-        print(f'Function {function.__qualname__} is taking about {toe:.5f} seconds.')
+            _toe = tac - _tic
+        funcs_results[function] = _toe
+        print(f'Function {function.__qualname__} is taking about {_toe:.5f} seconds.')
+        return to_return
     return new_func
